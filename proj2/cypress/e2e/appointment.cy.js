b90762539f55889e88ada754a170ee31894fae2e
Martan03 <martinslezak03@gmail.com>
2025-04-28 18:47:48 +0200

    Add all appointment scenario tests

--- proj2/cypress/e2e/appointment.cy.js
+++ proj2/cypress/e2e/appointment.cy.js
@@ -43,7 +43,7 @@ describe('Managing appointments', () => {
         cy.get(
             '#appointments-modal > .modal-dialog > .modal-content' +
             '> .modal-header > .modal-title'
-        ).click();
+        ).contains('Edit Appointment');
     });
 
     it('Editing an appointment (11)', () => {
@@ -57,5 +57,62 @@ describe('Managing appointments', () => {
         cy.get('#save-appointment').click();
         cy.get('.toast').should('be.visible');
         cy.get('.toast-body').contains('Appointment saved successfully');
+    });
+
+    it('Required fields highlighted (12)', () => {
+        navToAdmin();
+        appointmentDetails();
+        cy.get('.edit-popover').click();
+
+        cy.get('#first-name').clear();
+        cy.get('#last-name').clear();
+        cy.get('#email').clear();
+        cy.get('#phone-number').clear();
+
+        cy.get('#save-appointment').click();
+
+        cy.get('#first-name').should('have.class', 'is-invalid');
+        cy.get('#last-name').should('have.class', 'is-invalid');
+        cy.get('#email').should('have.class', 'is-invalid');
+        cy.get('#phone-number').should('have.class', 'is-invalid');
+    });
+
+    it('Closing the appointment details pop-up (13)', () => {
+        navToAdmin();
+        appointmentDetails();
+
+        cy.get('.close-popover').click();
+        cy.get('.popover').should('not.exist');
+    });
+
+    it('Cancel editing (14)', () => {
+        navToAdmin();
+        appointmentDetails();
+        cy.get('.edit-popover').click();
+
+        cy.get('#appointments-modal').should('be.visible');
+        cy.wait(500);
+        cy.get(
+            '#appointments-modal > .modal-dialog > .modal-content' +
+            '> .modal-footer > .btn-secondary'
+        ).click();
+    });
+
+    it('Appointment creator (15)', () => {
+        navToAdmin();
+
+        cy.get('.dropdown > .btn').click();
+        cy.get('#insert-appointment').click();
+
+        cy.get('#appointments-modal').should('be.visible');
+        cy.get(
+            '#appointments-modal > .modal-dialog > .modal-content' +
+            '> .modal-header > .modal-title'
+        ).contains('New Appointment');
+
+        cy.get('#first-name').should('have.value', '');
+        cy.get('#last-name').should('have.value', '');
+        cy.get('#email').should('have.value', '');
+        cy.get('#phone-number').should('have.value', '');
     });
 });
